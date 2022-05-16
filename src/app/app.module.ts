70aded5850c7baf33ce06e8f8d2b70caed4a4895
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-05-15 21:30:53 -0300

    feat: new home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HeaderPageComponent } from './components/header-page/header-page.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { PageRegisterComponent } from './page-register/page-register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HeaderPageComponent, BannerComponent, FooterComponent, ModalComponent, PageRegisterComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
